fb79c2b1c88d51d4af5573e1956203c9f0894ac8
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-02 20:23:42 -0800

    Leave Module API

---
+++ src/pages/api/leave-module.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "services/prisma";
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { moduleName } = req.body;
+
+  const module = await prisma.module.findUnique({
+    where: {
+      name: moduleName,
+    },
+  });
+  if (module == null) return res.status(500).json("Module DNE");
+  await prisma.module.update({
+    where: {
+      name: moduleName,
+    },
+    data: {
+      lastSeen: Date(),
+      usedBy: "N/A",
+    },
+  });
+  return res.status(200).json("Success");
+}
