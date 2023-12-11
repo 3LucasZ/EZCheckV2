bbf1c37214ce53f5b2214c3f6ae27483ae1bc526
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-10 23:42:18 -0800

    Update utils.ts

--- services/utils.ts
+++ services/utils.ts
@@ -2,7 +2,10 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { debugMode } from "./constants";
 
 export function getIPFromReq(req: NextApiRequest) {
-  var ip = req.socket.remoteAddress;
+  console.log("headers", req.headers);
+  console.log("socket.remoteAddress", req.socket.remoteAddress);
+  var ip: string =
+    (req.headers["x-real-ip"] as string) || req.socket.remoteAddress || "err";
   if (ip?.substring(0, 7) == "::ffff:") {
     ip = ip.substring(7);
   }
