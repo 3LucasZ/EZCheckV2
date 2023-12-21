485cb0f5c78ecfc4eac1e573a6b339bc5ee25b12
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-20 18:26:29 -0800

    migrate AdminWidget2 to use BaseWIdget2

--- components/Widget/AdminWidget2.tsx
+++ components/Widget/AdminWidget2.tsx
@@ -1,8 +1,7 @@
-import { DeleteIcon } from "@chakra-ui/icons";
-import { Box, Flex, IconButton, Text, useDisclosure } from "@chakra-ui/react";
+import { useToast } from "@chakra-ui/react";
 import Router from "next/router";
-import ConfirmDeleteModal from "../ConfirmDeleteModal";
-import { debugMode } from "services/constants";
+import BaseWidget2 from "./BaseWidget2";
+import { poster } from "services/poster";
 
 export type AdminProps = {
   id: number;
@@ -12,55 +11,21 @@ export type AdminProps = {
 const AdminWidget: React.FC<{
   admin: AdminProps;
 }> = ({ admin }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const handleDelete = async () => {
-    try {
-      const body = { id: admin.id };
-      if (debugMode) console.log(body);
-      const res = await fetch("/api/delete-admin", {
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body),
-      });
-      if (res.status == 500) {
-        alert("Error, id:" + admin.id);
-      } else {
-        //alert("Success");
-        Router.reload();
-      }
-    } catch (error) {
-      if (debugMode) console.error(error);
-    }
+  const toaster = useToast();
+  const handleRemove = async () => {
+    const body = { id: admin.id };
+    const res = await poster("/api/delete-admin", body, toaster);
+    if (res.status == 200) Router.reload();
   };
   return (
-    <Flex width="100%">
-      <Box
-        borderRadius="md"
-        bg="teal.300"
-        color="white"
-        px={4}
-        h={8}
-        roundedRight="none"
-        width="100%"
-      >
-        <Text width="100%">{admin.email}</Text>
-      </Box>
-      <IconButton
-        onClick={onOpen}
-        colorScheme="red"
-        aria-label="delete"
-        icon={<DeleteIcon />}
-        h={8}
-        roundedLeft="none"
-        borderRadius="md"
-      />
-      <ConfirmDeleteModal
-        isOpen={isOpen}
-        onClose={onClose}
-        name={" the admin: " + admin.email}
-        handleDelete={handleDelete}
-      />
-    </Flex>
+    <BaseWidget2
+      title={admin.email}
+      bg={"teal.300"}
+      bgHover={"teal.400"}
+      handleRemove={handleRemove}
+      invert={false}
+      isAdmin={true}
+    />
   );
 };
 
