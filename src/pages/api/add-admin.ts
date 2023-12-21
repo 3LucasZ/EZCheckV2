3c3767d6decea30ad5098504e55a9c7ec98e6928
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-20 20:16:50 -0800

    delete/add admin apis now log what happened under level 1

--- src/pages/api/add-admin.ts
+++ src/pages/api/add-admin.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import createLog from "services/createLog";
 import prisma from "services/prisma";
 import { prismaErrHandler } from "services/prismaErrHandler";
 
@@ -6,7 +7,8 @@ export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { email } = req.body;
+  const { creator, email } = req.body;
+  if (creator == "") return res.status(500).json("Creator can not be empty.");
   if (email == "") return res.status(500).json("Email can not be empty.");
   try {
     const op = await prisma.admin.create({
@@ -14,6 +16,7 @@ export default async function handle(
         email: email,
       },
     });
+    await createLog(creator + " created admin " + email, 1);
     return res.status(200).json(op);
   } catch (e) {
     return res.status(500).json(prismaErrHandler(e));
