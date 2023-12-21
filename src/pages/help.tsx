ab05fa1cf92deea3fb45a97a19ad9e9c8dd42d5a
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-21 00:38:46 -0800

    Create help.tsx

---
+++ src/pages/help.tsx
@@ -0,0 +1,75 @@
+import Layout from "components/Layout";
+import { GetServerSideProps } from "next";
+import { useSession } from "next-auth/react";
+import prisma from "services/prisma";
+
+import { checkAdmin } from "services/checkAdmin";
+
+import { Box, Link, ListItem, Text, UnorderedList } from "@chakra-ui/react";
+import { AdminProps } from "components/Widget/AdminWidget2";
+
+type PageProps = {
+  admins: AdminProps[];
+};
+export default function Home({ admins }: PageProps) {
+  const { data: session } = useSession();
+  const isAdmin = checkAdmin(session, admins);
+  return (
+    <Layout isAdmin={isAdmin}>
+      <Box px="5" overflowY="auto">
+        <Text fontSize="4xl">Version</Text>
+        <Text fontSize="xl">2.1 (Alpha)</Text>
+        <Text fontSize="4xl">Q&A</Text>
+        <Text fontSize="xl">
+          Q: Why is the website considered unsafe and untrusted by Safari and
+          Chrome?
+        </Text>
+        <Text fontSize="xl">
+          A: The website is in the alpha version and running on a local
+          development server. Rest assured, your data is safe. Check out our
+          privacy policy and terms of service below.
+        </Text>
+        <Text fontSize="xl">Q: What does the invert checkbox do?</Text>
+        <Text fontSize="xl">
+          A: When invert mode is on, the display will tell you what machines a
+          student does not have access to, and which students can not use a
+          machine.
+        </Text>
+        <Text fontSize={"4xl"}>Documents</Text>
+        <Link
+          color="teal.500"
+          href="https://www.freeprivacypolicy.com/live/867a55b1-f612-458c-a96b-73337e43fe99"
+          display={"block"}
+          fontSize={"xl"}
+        >
+          Privacy Policy
+        </Link>
+        <Link
+          color="teal.500"
+          href="https://www.freeprivacypolicy.com/live/a127aadd-459e-4134-89e1-c2773f78391f"
+          display={"block"}
+          fontSize={"xl"}
+        >
+          Terms of Service
+        </Link>
+        <Text fontSize="4xl">Help</Text>
+        <Text fontSize="xl">
+          Please do not hesitate to email us at sahuber@vcs.net if you have any
+          questions, need further instruction, or have suggestions for
+          improvement.
+        </Text>
+        <Box h="10px"></Box>
+      </Box>
+      <Box minH="calc(50px + env(safe-area-inset-bottom))"></Box>
+    </Layout>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const admins = await prisma.admin.findMany();
+  return {
+    props: {
+      admins: admins,
+    },
+  };
+};
