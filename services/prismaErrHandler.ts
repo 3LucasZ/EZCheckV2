538401388a0167352f03532673df98b1dcf777f6
3LucasZ <lucas.zheng@warriorlife.net>
2024-01-06 04:13:09 -0800

    update prismaErrHandler error messages: all lower case, no period at the end.

--- services/prismaErrHandler.ts
+++ services/prismaErrHandler.ts
@@ -15,24 +15,22 @@ export function prismaErrHandler(e: any): string {
     }
     if (e.code == "P2000") {
       //varChar constraint
-      return "Field " + e.meta?.column_name + " is too long.";
+      return "field " + e.meta?.column_name + " is too long";
     } else if (e.code == "P2002") {
       //unique constraint
       //e.meta.target
-      return (
-        "Another object with the same " + e.meta?.target + " exists already."
-      );
+      return e.meta?.target + " must be unique";
     } else if (e.code == "P2025") {
       //object DNE
       //e.meta.modelName
-      return "Your data is outdated, please refresh your webpage.";
+      return "your data is outdated, please refresh your webpage";
     } else if (e.code == "P2003") {
       //relation DNE
       //e.meta.modelName, e.meta.field_name
-      return "Your data is outdated, please refresh your webpage.";
+      return "your data is outdated, please refresh your webpage";
     } else {
       //unhandled prisma error
-      return "Your data is outdated, please refresh your webpage.";
+      return "your data is outdated, please refresh your webpage";
       //   return "Unhandled error: " + e.code + " | " + e.message;
     }
   } else {
