2517720faf63adda8601d4896e781e322046001e
3LucasZ <lucas.zheng@warriorlife.net>
2024-01-07 07:08:38 -0800

    Create home.tsx

---
+++ src/pages/admin/home.tsx
@@ -0,0 +1,67 @@
+import { Box, SimpleGrid } from "@chakra-ui/react";
+import { RouteButton } from "components/RouteButton";
+import Layout from "components/Layout";
+import { GetServerSideProps } from "next";
+import { useSession } from "next-auth/react";
+import prisma from "services/prisma";
+
+import { MdManageAccounts } from "react-icons/md";
+import { GiSewingMachine } from "react-icons/gi";
+import { IoDocumentText } from "react-icons/io5";
+import { IoIosInformationCircle } from "react-icons/io";
+
+import { checkAdmin } from "services/checkAdmin";
+import { AdminProps } from "components/Widget/AdminWidget2";
+
+type PageProps = {
+  admins: AdminProps[];
+};
+export default function Home({ admins }: PageProps) {
+  const { data: session } = useSession();
+  console.log(session);
+  const isAdmin = checkAdmin(session, admins);
+  return (
+    <Layout isAdmin={isAdmin}>
+      <SimpleGrid columns={[1, 2]} spacing={10} overflowY="auto" h={"100%"}>
+        <RouteButton
+          route={"manage-students"}
+          text={"Manage Students"}
+          icon={MdManageAccounts}
+          color={"teal.300"}
+          hoverColor={"teal.100"}
+        ></RouteButton>
+        <RouteButton
+          route={"manage-machines"}
+          text={"Manage Machines"}
+          icon={GiSewingMachine}
+          color={"blue.300"}
+          hoverColor={"blue.100"}
+        ></RouteButton>
+        <RouteButton
+          route={"logs"}
+          text={"View Logs"}
+          icon={IoDocumentText}
+          color={"orange.400"}
+          hoverColor={"orange.100"}
+        ></RouteButton>
+        <RouteButton
+          route={"help"}
+          text={"Help"}
+          icon={IoIosInformationCircle}
+          color={"red.400"}
+          hoverColor={"red.100"}
+        ></RouteButton>
+      </SimpleGrid>
+      <Box minH="calc(60px + env(safe-area-inset-bottom))"></Box>
+    </Layout>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const admins = await prisma.admin.findMany();
+  return {
+    props: {
+      admins: admins,
+    },
+  };
+};
