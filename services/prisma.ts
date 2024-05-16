fd50a894ce2ad434fd50b024218baaaec76b9de2
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-16 02:17:45 -0700

    Update prisma.ts

--- services/prisma.ts
+++ services/prisma.ts
@@ -1,5 +1,38 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
-let prisma = new PrismaClient();
+const prismaExtension = Prisma.defineExtension({
+  result: {
+    user: {
+      createdAt: {
+        needs: {
+          createdAt: true,
+        },
+        compute(user) {
+          return user.createdAt.toString();
+        },
+      },
+      updatedAt: {
+        needs: {
+          createdAt: true,
+        },
+        compute(user) {
+          return user.createdAt.toString();
+        },
+      },
+    },
+  },
+});
+
+const prismaClientSingleton = () => {
+  return new PrismaClient().$extends(prismaExtension);
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
