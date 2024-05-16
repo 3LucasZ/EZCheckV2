0698658baf66defef5472480bf801f657d507973
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-16 02:17:35 -0700

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
+import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
+import prisma from "services/prisma";
+
 const { GOOGLE_ID = "", GOOGLE_SECRET = "" } = process.env;
 
 // export default NextAuth({
@@ -14,18 +18,34 @@ const { GOOGLE_ID = "", GOOGLE_SECRET = "" } = process.env;
 
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
   const { host } = req.headers;
-
   if (!host) return res.status(400).send(`Bad Request, missing host header`);
   //else return res.status(400).send("Debug: " + host);
 
+  // process.env.NEXTAUTH_URL = "https://" + host + "/ezfind/api/auth";
   process.env.NEXTAUTH_URL = "https://" + host;
 
   return NextAuth({
+    adapter: PrismaAdapter(prisma) as Adapter,
     providers: [
       GoogleProvider({
         clientId: GOOGLE_ID,
         clientSecret: GOOGLE_SECRET,
       }),
     ],
+    callbacks: {
+      //Add data to user object so it is passed along with session
+      session({ session, token, user }) {
+        //id
+        session.user.id = user.id;
+        //isAdmin
+        session.user.isAdmin = user.isAdmin;
+        if (
+          user.email == "lucas.j.zheng@gmail.com" ||
+          user.email == "lucas.zheng@warriorlife.net"
+        )
+          session.user.isAdmin = true;
+        return session;
+      },
+    },
   })(req, res);
 }
