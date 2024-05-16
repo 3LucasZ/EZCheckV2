b8544a44f58a13fabedf93018c2a0a9124c06138
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-16 02:17:17 -0700

    new. constant styles

--- services/constants.ts
+++ services/constants.ts
@@ -1,2 +1,5 @@
 export const debugMode = true;
 export const PINLen = 10;
+
+export const responsivePx = [2, "5vw", "10vw", "15vw"];
+export const responsiveHeaderFontSize = ["2xl", "2xl", "2xl", "3xl", "4xl"];
