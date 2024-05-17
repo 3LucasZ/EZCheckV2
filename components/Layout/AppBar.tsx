0b8bc7c13e4d93097af39837c0e41602d50b3290
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-17 01:28:42 -0700

    Banger AppBar

--- components/Layout/AppBar.tsx
+++ components/Layout/AppBar.tsx
@@ -1,28 +1,52 @@
-import { HStack, Icon, IconButton } from "@chakra-ui/react";
+import { Box, Button, Center, HStack, Icon } from "@chakra-ui/react";
 import { FaHome } from "react-icons/fa";
 import { MdManageAccounts } from "react-icons/md";
 import { GiSewingMachine } from "react-icons/gi";
 import { IoDocumentText } from "react-icons/io5";
-import { IoIosInformationCircle } from "react-icons/io";
 
-import AppBarBtn from "./AppBarBtn";
 import { BiSolidWrench } from "react-icons/bi";
+import { IconType } from "react-icons";
+import Router from "next/router";
 
 export default function AppBar() {
   return (
-    <HStack
-      position="fixed"
-      bottom="0"
-      w="100%"
-      bg="teal.300"
-      h={"calc(50px + env(safe-area-inset-bottom))"}
-      p={0}
+    <Box position="fixed" bottom="0" w="100%">
+      <HStack gap={0} h={"calc(50px + env(safe-area-inset-bottom))"}>
+        <AppBarBtn icon={FaHome} href="/admin/home" />
+        <AppBarBtn icon={MdManageAccounts} href="/admin/manage-students" />
+        <AppBarBtn icon={GiSewingMachine} href="/admin/manage-machines" />
+        <AppBarBtn icon={IoDocumentText} href="/admin/view-logs" />
+        <AppBarBtn icon={BiSolidWrench} href="/admin/config" />
+      </HStack>
+    </Box>
+  );
+}
+
+type AppBarBtnProps = {
+  icon: IconType;
+  href: string;
+};
+function AppBarBtn({ icon, href }: AppBarBtnProps) {
+  return (
+    <Box
+      w={"100%"}
+      h={"100%"}
+      aria-label={""}
+      pt="13px"
+      onClick={() => Router.push(href)}
+      style={{ textDecoration: "none" }}
+      sx={{
+        WebkitUserDrag: "none",
+      }}
+      bgGradient="linear(to-b, orange.200, red.300)"
+      color="white"
+      _hover={{ color: "red.300" }}
     >
-      <AppBarBtn icon={FaHome} href="/admin/home" />
-      <AppBarBtn icon={MdManageAccounts} href="/admin/manage-students" />
-      <AppBarBtn icon={GiSewingMachine} href="/admin/manage-machines" />
-      <AppBarBtn icon={IoDocumentText} href="/admin/view-logs" />
-      <AppBarBtn icon={BiSolidWrench} href="/admin/config" />
-    </HStack>
+      {
+        <Center>
+          <Icon as={icon} boxSize={6} />
+        </Center>
+      }
+    </Box>
   );
 }
