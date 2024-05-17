461718ea8fe7d3eb809b9f0f0c954d8dc61d0d06
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-17 01:38:53 -0700

    Fix AppBar UI

--- components/Layout/AppBar.tsx
+++ components/Layout/AppBar.tsx
@@ -1,22 +1,18 @@
-import { Box, Button, Center, HStack, Icon } from "@chakra-ui/react";
-import { FaHome } from "react-icons/fa";
-import { MdManageAccounts } from "react-icons/md";
-import { GiSewingMachine } from "react-icons/gi";
-import { IoDocumentText } from "react-icons/io5";
+import { Box, Center, HStack, Icon } from "@chakra-ui/react";
 
-import { BiSolidWrench } from "react-icons/bi";
 import { IconType } from "react-icons";
 import Router from "next/router";
+import { FiCpu, FiFileText, FiHome, FiTool, FiUsers } from "react-icons/fi";
 
 export default function AppBar() {
   return (
     <Box position="fixed" bottom="0" w="100%">
       <HStack gap={0} h={"calc(50px + env(safe-area-inset-bottom))"}>
-        <AppBarBtn icon={FaHome} href="/admin/home" />
-        <AppBarBtn icon={MdManageAccounts} href="/admin/manage-students" />
-        <AppBarBtn icon={GiSewingMachine} href="/admin/manage-machines" />
-        <AppBarBtn icon={IoDocumentText} href="/admin/view-logs" />
-        <AppBarBtn icon={BiSolidWrench} href="/admin/config" />
+        <AppBarBtn icon={FiHome} href="/admin/home" />
+        <AppBarBtn icon={FiUsers} href="/admin/manage-students" />
+        <AppBarBtn icon={FiCpu} href="/admin/manage-machines" />
+        <AppBarBtn icon={FiFileText} href="/admin/view-logs" />
+        <AppBarBtn icon={FiTool} href="/admin/config" />
       </HStack>
     </Box>
   );
@@ -40,7 +36,7 @@ function AppBarBtn({ icon, href }: AppBarBtnProps) {
       }}
       bgGradient="linear(to-b, orange.200, red.300)"
       color="white"
-      _hover={{ color: "red.300" }}
+      _hover={{ color: "red.400" }}
     >
       {
         <Center>
