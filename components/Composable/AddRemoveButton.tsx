4396dd237a3b7e7a6c1ae89b6143fce7bccde5a1
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-17 00:53:08 -0700

    Create AddRemoveButton.tsx

---
+++ components/Composable/AddRemoveButton.tsx
@@ -0,0 +1,60 @@
+//COPY FROM EZFIND2
+import { SmallAddIcon, SmallCloseIcon } from "@chakra-ui/icons";
+import { AspectRatio, Icon, IconButton, useDisclosure } from "@chakra-ui/react";
+import ConfirmActionModal from "components/Main/ConfirmActionModal";
+import { MouseEventHandler } from "react";
+import { FiHome, FiMinus, FiPlus, FiX } from "react-icons/fi";
+
+type AddRemoveButtonProps = {
+  mode: number;
+  invisible?: boolean;
+  handleAdd?: Function;
+  handleRemove?: Function;
+  askConfirmation?: boolean;
+  actionStr?: string;
+};
+export default function AddRemoveButton({
+  mode,
+  invisible,
+  handleAdd,
+  handleRemove,
+  askConfirmation,
+  actionStr,
+}: AddRemoveButtonProps) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const action = () =>
+    mode == 1 ? handleAdd && handleAdd() : handleRemove && handleRemove();
+  return (
+    <>
+      <Icon
+        //---color
+        bg="white"
+        transition=""
+        _hover={{ bg: mode == 1 ? "teal.300" : "red.400", color: "white" }}
+        color="black"
+        //---border
+        rounded="xl"
+        borderColor={"grey.200"}
+        borderWidth={"1px"}
+        //---display
+        as={mode == 1 ? FiPlus : FiX}
+        display={mode == 0 ? "none" : ""}
+        opacity={invisible ? 0 : 1}
+        boxSize={"10"}
+        p="2.5"
+        //---misc
+        onClick={(e) => {
+          e.stopPropagation(); //if parent element is clicked, it will be ignored, so that this action activates instead.
+          askConfirmation ? onOpen() : action();
+        }}
+        aria-label={""}
+      />
+      <ConfirmActionModal
+        isOpen={isOpen}
+        onClose={onClose}
+        protectedAction={action}
+        actionStr={actionStr}
+      />
+    </>
+  );
+}
