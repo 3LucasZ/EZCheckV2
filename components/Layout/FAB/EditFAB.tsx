3fb87d457fae77ee07a797be739ff8d4b89f066d
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-17 01:54:20 -0700

    FAB import from EZFind

---
+++ components/Layout/FAB/EditFAB.tsx
@@ -0,0 +1,24 @@
+//COPY FROM EZFIND2
+import { FAB } from "./FAB";
+import { FiCheck, FiEdit2, FiX } from "react-icons/fi";
+
+type EditFABProps = {
+  isEdit: boolean;
+  onEdit: Function;
+  onSave: Function;
+  onCancel: Function;
+};
+
+export const EditFAB = ({ isEdit, onEdit, onSave, onCancel }: EditFABProps) => {
+  return (
+    <>
+      <FAB icon={FiCheck} onClick={onSave} bottomOffset={isEdit ? 75 : 0} />
+      <FAB
+        icon={isEdit ? FiX : FiEdit2}
+        onClick={isEdit ? onCancel : onEdit}
+        bg={isEdit ? "red.300" : ""}
+        hoverBg={isEdit ? "red.400" : ""}
+      />
+    </>
+  );
+};
---
+++ components/Layout/FAB/FAB.tsx
@@ -0,0 +1,61 @@
+//COPY FROM EZFIND2
+import {
+  Icon,
+  Text,
+  HStack,
+  ComponentWithAs,
+  IconProps,
+} from "@chakra-ui/react";
+import { IconType } from "react-icons";
+
+type FABProps = {
+  onClick: Function;
+  icon: IconType | ComponentWithAs<"svg", IconProps>;
+  name?: string;
+  bg?: string;
+  hoverBg?: string;
+  bottomOffset?: number;
+  hidden?: boolean;
+};
+
+export const FAB = ({
+  icon,
+  onClick,
+  name,
+  bg,
+  hoverBg,
+  bottomOffset,
+  hidden,
+}: FABProps) => {
+  !bottomOffset && (bottomOffset = 0);
+  return (
+    <HStack
+      //---position
+      position={"fixed"}
+      right={"8px"}
+      bottom={`calc(58px + env(safe-area-inset-bottom) + ${bottomOffset}px)`}
+      //---sizing + spacing
+      minH="16"
+      minW="16"
+      maxW={name ? "" : "16"}
+      py={name ? "3" : "auto"}
+      px={name ? "6" : "auto"}
+      //---looks
+      bg={bg ? bg : "teal.300"}
+      _hover={{ bg: hoverBg ? hoverBg : "teal.400" }}
+      color="white"
+      rounded="full"
+      // boxShadow={"2xl"}
+      //---misc
+      onClick={(e) => onClick()}
+      zIndex={100}
+      transition={"bottom 0.3s"}
+      hidden={hidden}
+    >
+      <Icon as={icon} boxSize="6" p="0" m="0" w="100%" />
+      <Text fontSize={"2xl"} hidden={name ? false : true}>
+        {name}
+      </Text>
+    </HStack>
+  );
+};
