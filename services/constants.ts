dd10cc1064c148da468c51669729edf96db37204
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-21 00:21:43 -0700

    PINLen 10 -> 6

--- services/constants.ts
+++ services/constants.ts
@@ -1,5 +1,5 @@
 export const debugMode = true;
-export const PINLen = 10;
+export const PINLen = 6;
 
 export const responsivePx = [2, "5vw", "10vw", "15vw"];
 export const responsiveHeaderFontSize = ["2xl", "2xl", "2xl", "3xl", "4xl"];
